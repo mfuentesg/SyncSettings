590e6e0e258031aac22812510b9898a710a30324
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2015-07-31 00:53:17 -0400

    define the basic behavior for this command

--- sync_settings/commands/upload.py
+++ sync_settings/commands/upload.py
@@ -3,7 +3,19 @@
 import sublime
 from sublime_plugin import WindowCommand
 from ..sync_settings_manager import SyncSettingsManager
+from ..gistapi import Gist
 
 class SyncSettingsUploadCommand (WindowCommand):
 	def run (self):
-		pass
+		gistId = SyncSettingsManager.settings('gist_id')
+		if gistId:
+			try:
+				api = Gist(SyncSettingsManager.settings('access_token'))
+				data = {'files': SyncSettingsManager.getContentFiles() }
+				print(data)
+				api.edit(gistId, data)
+				sublime.status_message('Sync Settings: Your files was uploaded successfully!')
+			except Exception as e:
+				sublime.status_message('Sync Settings: ' + str(e))
+		else:
+			sublime.status_message('Sync Settings: Set the gist_id in the configuration file')
