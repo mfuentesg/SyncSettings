a4549df2df1690ce80bbc216c5558d6169156ba8
Marcelo Fuentes <marcelo@classroom.tv>
2015-08-11 15:06:29 -0300

    add about command

--- sync_settings/commands/__init__.py
+++ sync_settings/commands/__init__.py
@@ -1,9 +1,11 @@
 from .create_and_upload import SyncSettingsCreateAndUploadCommand
 from .download import SyncSettingsDownloadCommand
 from .upload import SyncSettingsUploadCommand
+from .about import SyncSettingsAboutCommand
 
 __all__ = [
   "SyncSettingsCreateAndUploadCommand",
   "SyncSettingsDownloadCommand",
-  "SyncSettingsUploadCommand"
+  "SyncSettingsUploadCommand",
+  "SyncSettingsAboutCommand"
 ]
