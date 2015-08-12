e2f11958351dc19907705235ea5098b1247066f7
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2015-08-11 22:44:47 -0400

    add simple functions to logger file

--- sync_settings/logger.py
+++ sync_settings/logger.py
@@ -2,19 +2,31 @@ import os, time
 
 class Logger:
 	FILE_NAME = '.sync-settings.log'
-	MESSAGE_ERROR_TYPE = 1
 	MESSAGE_INFO_TYPE = 1
+	MESSAGE_ERROR_TYPE = 2
 
 	@staticmethod
-	def log (message, type = None):
-		if not type is None:
-			pass
+	def log (message, type):
+		if type == Logger.MESSAGE_ERROR_TYPE:
+			message = 'ERROR: ' + message
+		elif type == Logger.MESSAGE_INFO_TYPE:
+			message = 'INFO: ' + message
+		Logger.write(message)
 
 	@staticmethod
 	def getPath ():
-		return os.path.expanduser('~', Logger.FILE_NAME)
+		return os.path.join(os.path.expanduser('~'), Logger.FILE_NAME)
 
 	@staticmethod
 	def write (message):
 		fullTime = time.strftime("[%d/%m/%Y - %H:%M:%S] ")
 		message = fullTime + message
+		path = Logger.getPath()
+		action = 'a+' if os.path.exists(path) else 'w+'
+
+		try:
+			with open(path, action) as f:
+				f.write(message + '\n')
+				f.close()
+		except Exception as e:
+			print(e)
