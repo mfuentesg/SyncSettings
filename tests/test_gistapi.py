40cfe15fef0788ece10d5d7c643bc3a1254afc19
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2015-08-23 21:57:50 -0400

    add tests for gistapi and logger modules

--- tests/test_gistapi.py
+++ tests/test_gistapi.py
@@ -1,4 +1,117 @@
+# -*- coding: utf-8 -*-
+
+from tests import *
 from unittest import TestCase
 
+def getGist (access_token = ''):
+	return gistapi.Gist(access_token)
+
+api = gistapi.Gist(Opts.get('access_token'))
+test_gist = api.create({
+	'files': {
+		'test_gist.txt': {
+			'content': 'Gist test content'
+		}
+	}
+})
+
 class TestGistAPI (TestCase):
-	pass
+	def test_access_token (self):
+		with self.assertRaises(gistapi.GistException):
+			getGist()
+		self.assertIsInstance(api, gistapi.Gist)
+		self.assertIsNotNone(api)
+
+	def test_create_gist (self):
+		data = {'description': 'test description'}
+
+		# Try to create a gist with invalid data
+		# Without files object
+		with self.assertRaises(gistapi.GistException):
+			api.create(data)
+		# With a wrong 'files' object
+		with self.assertRaises(gistapi.GistException):
+			api.create({
+				'description': 'some description',
+				'files': 'some files'
+			})
+
+		# Create a gist without description
+		gist = api.create({
+			'files': {
+				'file.txt': {
+					'content': 'content for this file.txt'
+				}
+			}
+		})
+		self.assertIsNotNone(gist.get('id'))
+		# Delete test gist
+		self.assertTrue(api.delete(gist.get('id')))
+
+		# Create a gist with description and files
+		data.update({ 'files': {
+			'someFile': {
+				'content': 'Content of this file'
+			}
+		}})
+		gist = api.create(data)
+		self.assertIsNotNone(gist.get('id'))
+		gist_id = gist.get('id')
+
+		# Get a list with all public gists
+		gist_items = api.list()
+
+		# Check if the created isn't public
+		for gist_item in gist_items:
+			self.assertNotEqual(gist_item.get('id'), gist_id)
+		# Delete test gist
+		self.assertTrue(api.delete(gist_id))
+
+	def test_edit_gist (self):
+		# Get possible errors
+		# Passing wrong parameters
+		test_gist_id = test_gist.get('id')
+		with self.assertRaises(gistapi.GistException):
+			api.edit('someId', {})
+		# With wrong files object
+		with self.assertRaises(gistapi.GistException):
+			api.edit(test_gist_id, {
+				'files': 'some content'
+			})
+
+		# Updating without changes
+		gist = api.edit(test_gist_id, {})
+		self.assertIsNotNone(gist.get('id'))
+		self.assertEqual(len(gist.get('files')), 1)
+
+		# Adding a new file
+		gist = api.edit(test_gist_id, {
+			'files': { 'other_file.txt': {
+				'content': 'Some content'
+			}}
+		})
+		self.assertIsNotNone(gist.get('id'))
+		self.assertEqual(len(gist.get('files')), 2)
+
+		# Removing the created file
+		gist = api.edit(test_gist_id, {
+			'files': { 'other_file.txt': None}
+		})
+		self.assertIsNotNone(gist.get('id'))
+		self.assertEqual(len(gist.get('files')), 1)
+
+	def test_get_gist (self):
+		test_gist_id = test_gist.get('id')
+		# Getting errors
+		with self.assertRaises(gistapi.GistException):
+			api.get('---')
+
+		# Getting gist
+		gist = api.get(test_gist_id)
+		self.assertIsNotNone(gist.get('id'))
+		self.assertEqual(test_gist_id, gist.get('id'))
+		self.assertTrue(api.delete(test_gist_id))
+
+	def test_current_release (self):
+		release = api.getCurrentRelease()
+		self.assertIsNotNone(release.get('id'))
--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,4 +1,28 @@
+# -*- coding: utf-8 -*-
+
 from unittest import TestCase
+from tests import *
+import os
+
+_logger = logger.Logger
+_logger.FILE_NAME = Opts.get('logger_filename')
+test_path = os.path.join(os.path.expanduser('~'), Opts.get('logger_filename'))
 
 class TestLogger (TestCase):
-	pass
+	def test_filename (self):
+		self.assertEqual(_logger.FILE_NAME, Opts.get('logger_filename'))
+
+	def test_get_path (self):
+		self.assertEqual(_logger.getPath(), test_path)
+
+	def test_create_file (self):
+		self.assertFalse(os.path.exists(test_path))
+		_logger.createEmptyFile()
+		self.assertTrue(os.path.exists(test_path))
+		self.assertEqual(os.path.getsize(test_path), 0)
+
+	def test_log (self):
+		_logger.log('Some content', _logger.MESSAGE_ERROR_TYPE)
+		self.assertGreater(os.path.getsize(test_path), 0)
+		os.remove(test_path)
+		self.assertFalse(os.path.exists(test_path))
