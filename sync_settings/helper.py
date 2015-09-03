075d0d763f75f2fb272c5dcf7eb8883a05d83e14
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2015-09-03 00:30:14 -0400

    set base function for filter by pattern

--- sync_settings/helper.py
+++ sync_settings/helper.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os
+import os, re
 
 def getDifference (setA, setB):
   return list(filter(lambda el: el not in setB, setA))
@@ -10,22 +10,46 @@ def getHomePath (fl = ""):
     return joinPath((os.path.expanduser('~'), fl))
   return os.path.expanduser('~')
 
-def existsPath(path = '', isFolder = False):
+def existsPath(path, isFolder = False):
   opath = os.path
-  if isinstance(path, str) and opath.exists(path):
+  if isinstance(path, str) and path != "" and opath.exists(path):
     if (isFolder and opath.isdir(path)): return True
     if (not isFolder and opath.isfile(path)): return True
   return False
 
-def joinPath (list):
-  if isinstance(list, tuple) and len(list) > 1:
-    return os.path.join(*list)
+"""
+  @param tuple pathTuple
+  @return str|None
+"""
+def joinPath (pathTuple):
+  if isinstance(pathTuple, tuple) and len(pathTuple) > 1:
+    return os.path.join(*pathTuple)
   return None
 
-def getFiles (path = ''):
+def getFiles (path):
   if existsPath(path, True):
     f = []
     for root, dirs, files in os.walk(path):
       f.extend([joinPath((root, file)) for file in files])
     return f
   return []
+
+"""
+  @param list elements
+  @param list patterns
+  @return list
+"""
+def filterByPatterns (elements, patterns = []):
+  isValidElements = isinstance(elements, list) and len(elements) > 0
+  isValidPattern = isinstance(patterns, list) and len(patterns) > 0
+  if validElements and validPattern:
+    for element in elements:
+      for pattern in patterns:
+        """
+          Verify if the pattern is a:
+          * extension file
+          * regex
+          * filename
+        """
+        pass
+  return list
