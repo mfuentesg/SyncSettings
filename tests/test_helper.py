eb4cff1a4449becb20f9900eb7fc73856c8fbd1f
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2015-09-03 00:06:33 -0400

    remove double type cast

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -6,7 +6,7 @@ import os, shutil
 
 class TestHelper (TestCase):
   def test_difference (self):
-    l = (list(helper.getDifference([1, 2, 3, 4], [1, 2, 3])))
+    l = helper.getDifference([1, 2, 3, 4], [1, 2, 3])
     self.assertEqual(len(l), 1)
     self.assertEqual(l[0], 4)
 
