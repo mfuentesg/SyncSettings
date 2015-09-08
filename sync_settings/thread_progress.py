e7bc2036bc8a238933e1b87eb52c1c0e62bf060e
Marcelo Fuentes <marcelo@classroom.tv>
2015-09-08 17:45:59 -0300

    use status_message method

--- sync_settings/thread_progress.py
+++ sync_settings/thread_progress.py
@@ -23,37 +23,23 @@ class ThreadProgress():
     self.success_message = success_message
     self.addend = 1
     self.size = 8
-    self.last_view = None
-    self.window = None
 
     self.thread = threading.Thread(target=thread_target)
     self.thread.start()
     sublime.set_timeout(lambda: self.run(0), 100)
 
   def run(self, i):
-    if self.window is None:
-      self.window = sublime.active_window()
-    active_view = self.window.active_view()
-
-    if self.last_view is not None and active_view != self.last_view:
-      self.last_view.erase_status('_sync_settings')
-      self.last_view = None
-
     if not self.thread.is_alive():
-      active_view.set_status('_sync_settings', self.success_message)
+      sublime.status_message(self.success_message)
       return
 
     before = i % self.size
     after = (self.size - 1) - before
 
-    active_view.set_status('_sync_settings', '%s [%s=%s]' % (self.message, ' ' * before, ' ' * after))
-    if self.last_view is None:
-      self.last_view = active_view
+    sublime.status_message('%s [%s=%s]' % (self.message, ' ' * before, ' ' * after))
 
-    if not after:
-      self.addend = -1
-    if not before:
-      self.addend = 1
+    if not after: self.addend = -1
+    if not before: self.addend = 1
+
     i += self.addend
-
     sublime.set_timeout(lambda: self.run(i), 100)
