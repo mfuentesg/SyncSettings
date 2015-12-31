005d7316cd56ae22940d93f1b0286addd8e6fe2c
Marcelo Fuentes <marceloe.fuentes@gmail.com>
2015-12-31 10:48:58 -0300

    create generic function to get a gist api object

--- sync_settings/commands/upload.py
+++ sync_settings/commands/upload.py
@@ -2,7 +2,6 @@
 
 import sublime
 from sublime_plugin import WindowCommand
-from ..libs.gistapi import Gist
 from ..sync_settings_manager import SyncSettingsManager as Manager
 from ..thread_progress import ThreadProgress
 
@@ -10,18 +9,23 @@ class SyncSettingsUploadCommand(WindowCommand):
   def run(self):
     def upload_request():
       gist_id = Manager.settings('gist_id')
+
       if gist_id:
         try:
-          api = Gist(Manager.settings('access_token'))
-          files = Manager.get_files_content()
-          if len(files) > 0:
-            data = {'files': files}
-            api.edit(gist_id, data)
-            Manager.show_message_and_log('Your files was uploaded successfully!', False)
-          else:
-            Manager.show_message_and_log('There are not enough files to upload', False)
+          api = Manager.gist_api()
+
+          if api is not None:
+            files = Manager.get_files_content()
+
+            if len(files) > 0:
+              data = {'files': files}
+              api.edit(gist_id, data)
+              Manager.show_message_and_log('Your files was uploaded successfully!', False)
+            else:
+              Manager.show_message_and_log('There are not enough files to upload', False)
         except Exception as e:
           Manager.show_message_and_log(e)
       else:
-        Manager.show_message_and_log('Set the gist_id in the configuration file', False)
+        Manager.show_message_and_log('Set gist_id property on the configuration file', False)
+
     ThreadProgress(lambda: upload_request(), 'Uploading files')
